cecd4960afa551d41e8e120c332205426d43c909
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-10-17 22:32:44 +0100

    Convert Config to functional component

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,48 +1,44 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 
 import { NumberControl } from "./NumberControl";
 
-export class Config extends Component {
-	static propTypes = {
-		config: PropTypes.shape({
-			digits: PropTypes.number,
-			min: PropTypes.number,
-			max: PropTypes.number,
-		}).isRequired,
-		onChange: PropTypes.func.isRequired,
-	};
-
-	handleChange = (prop) => (event) => {
-		const { config, onChange } = this.props;
+export function Config(props) {
+	const { config, onChange } = props;
+	const handleChange = (prop) => (event) => {
 		onChange({ ...config, [prop]: parseInt(event.target.value) });
 	};
-
-	render() {
-		const { config } = this.props;
-		return (
-			<>
-				<NumberControl
-					id="minLength"
-					label="Minimum word length"
-					onChange={this.handleChange("min")}
-					value={config.min}
-				/>
-				<NumberControl
-					id="maxLength"
-					label="Maximum word length"
-					onChange={this.handleChange("max")}
-					value={config.max}
-				/>
-				<NumberControl
-					id="digits"
-					label="Number of digits"
-					onChange={this.handleChange("digits")}
-					value={config.digits}
-				/>
-			</>
-		);
-	}
+	return (
+		<>
+			<NumberControl
+				id="minLength"
+				label="Minimum word length"
+				onChange={handleChange("min")}
+				value={config.min}
+			/>
+			<NumberControl
+				id="maxLength"
+				label="Maximum word length"
+				onChange={handleChange("max")}
+				value={config.max}
+			/>
+			<NumberControl
+				id="digits"
+				label="Number of digits"
+				onChange={handleChange("digits")}
+				value={config.digits}
+			/>
+		</>
+	);
 }
+
+Config.propTypes = {
+	config: PropTypes.shape({
+		digits: PropTypes.number,
+		min: PropTypes.number,
+		max: PropTypes.number,
+	}).isRequired,
+	onChange: PropTypes.func.isRequired,
+};
 
 export default Config;
