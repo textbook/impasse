6a0c06b99a12da7862dfc87df4bd558b80fbf9d6
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-12-24 09:12:17 +0000

    Use digits parameter to determine length

--- server/password.js
+++ server/password.js
@@ -4,7 +4,7 @@ const SYMBOLS = "!@#$%^&*";
 
 export default (words, digits = 2) => {
 	const [word1, word2] = sample(words, 2);
-	const number = `${randRange(0, 10 ** digits)}`.padStart(2, "0");
+	const number = `${randRange(0, 10 ** digits)}`.padStart(digits, "0");
 	const symbol = choice(SYMBOLS);
 	return `${word1}${number}${word2}${symbol}`;
 };
