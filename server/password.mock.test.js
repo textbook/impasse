bb4508a9f302a8c16610df2c924270f3eccc4514
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-12-24 09:09:51 +0000

    Add test to consistently fail on zero padding
    The number of digits padded to is fixed so using a non-default digit count can generate passwords that don't meet requirements.

--- server/password.mock.test.js
+++ server/password.mock.test.js
@@ -17,4 +17,15 @@ describe("password", () => {
 		expect(randRange).toHaveBeenCalledWith(0, 100);
 		expect(sample).toHaveBeenCalledWith(words, 2);
 	});
+
+	it("pads the non-default length number with zeroes", () => {
+		const words = ["alphabet", "barbaric"];
+		choice.mockReturnValue("!");
+		randRange.mockReturnValue(1);
+		sample.mockReturnValue(words);
+
+		const actual = password(words, 3);
+
+		expect(actual).toBe("alphabet001barbaric!");
+	});
 });
