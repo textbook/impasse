dc2ed2b9802a32e6ee7296ec47889c8d2d653c9a
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-04-23 23:14:47 +0100

    Show version in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,15 @@
+import packageData from "../../../package.json";
 import "./Footer.scss";
 import madeWithBulma from "./made-with-bulma.png";
+
+const { author: { name, url }, version } = packageData;
 
 export default function Footer() {
 	return (
 		<footer className="footer">
 			<div className="content has-text-centered">
 				<p>
-					Developed by <a href="https://github.com/textbook">Jonathan Sharpe</a>.
+					<span>Version {version}</span>, developed by <a href={url}>{name}</a>.
 					<a className="bulma-logo" href="https://bulma.io">
 						<img src={madeWithBulma} alt="Made with Bulma" />
 					</a>
---
+++ client/src/components/Footer.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+
+import Footer from "./Footer";
+
+jest.mock("../../../package.json", () => ({
+	author: {
+		name: "Jane Doe",
+		email: "jane.doe@morgue.org",
+		url: "https://morgue.org",
+	},
+	version: "1.2.3",
+}));
+
+describe("Footer component", () => {
+	it("renders author information", () => {
+		render(<Footer />);
+		expect(screen.getByText("Jane Doe")).toHaveAttribute("href", "https://morgue.org");
+	});
+
+	it("renders version information", () => {
+		render(<Footer />);
+		expect(screen.queryByText("Version 1.2.3")).toBeInTheDocument();
+	});
+});
